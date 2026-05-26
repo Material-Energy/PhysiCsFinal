04f43ecb77394a516aef8abee9909721655c6d3e
Brian Zheng <zhengbrian1@gmail.com>
2026-05-26 09:43:45 -0400

    commutator, but in wrong place

--- program.py
+++ program.py
@@ -11,6 +11,7 @@ Web VPython 3.2
 scene.forward = vec(0, -1, 0)
 width = 5
 height = 3
+bronze = vec(1,0.7,0.2)
 
 path = paths.rectangle(width=width, height = height)
 #print(path)
@@ -20,5 +21,16 @@ path[-1] = path[-2] + vec(width/4, 0, 0)
 path.insert(0, path[0]+vec(0, 0, height))
 path.append(path[-1]+vec(0, 0, height))
 
-wire = curve(pos=path, radius = 0.1, color=vec(1,0.7,0.2))
-print(wire.slice(0, wire.npoint)[0])
+for i in range(len(path)):
+    path[i] = rotate(path[i], axis=vector(1, 0, 0), angle=-3*pi/4)
+    
+wire = curve(pos=path, radius = 0.1, color=bronze)
+
+#print(wire.slice(0, wire.npoint)[0])
+print(wire.point(0)['pos'])
+
+
+arc = paths.arc(pos=wire.point(0)['pos'] + vec(0, 0, 1), radius = width/4, angle1=-pi/24, angle2=-23*pi/24)
+commutator = extrusion(shape=shapes.rectangle(width=0.1, height=2), path=arc, color=bronze)
+commutator.rotate(axis=vector(1, 0, 0), angle=-pi/6)
+commutator.rotate(axis=wire.point(wire.npoints - 1)['pos'], angle=pi/2)
