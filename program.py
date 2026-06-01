93bbf6b27469b71364535d49a5f53143d34e7fdd
Material-Energy <material.energitics@gmail.com>
2026-06-01 10:14:50 -0400

    aattempt to debug back emf

--- program.py
+++ program.py
@@ -1,17 +1,17 @@
 Web VPython 3.2
 
 B = 1.0 # magnitude of mangetic field from external magnets
-V = 5.0 # source voltage
-R = 10 # resistance of wire
+V = 10.0 # source voltage
+R = 1 # resistance of wire
 
 B_vec = vec(1, 0, 0)
 lf = vec(0, 0, 0)
 rf = vec(0, 0, 0)
 
 i_wire = V / R
-L = 3 # height of loop
-r = 2.5 # radius of loop
-I = 2.0 # moment of inertia of the armature
+L = 3 # height of loop (cm)
+r = 2.5 # radius of loop (cm)
+I = 1e-3 # moment of inertia of the armature
 
 t = 0
 dt = 0.01
@@ -82,36 +82,46 @@ commutators = compound([commutator_left, commutator_right])
 
 
 # physics stuff
-A = width * height
+A = width * height * 0.01 ** 2
  
 theta = 0
 omega = 0
 
 RPM_graph = graph(title = "RPM vs time", xtitle = "t", ytitle = "RPM")
-gd = gdots()
+gd1 = gdots()
+
+flux_graph = graph(title = "Flux vs time", xtitle = "t", ytitle = "Flux")
+gd2 = gdots()
 
 
-while t < 10:
+while t < 1000:
     t += dt
     rate (1 / dt)
     
-    V_back = B*A*omega*sin(theta)
+    V_back = abs(B*A*omega*sin(theta))
+    print(f"{V_back:.4f}")
     
-    domega_dt = (r*B*L) / (I*R) * (V - V_back)
+    L /= 100
+    r /= 100
+    
+    i_loop = i_wire - V_back / R
+    print(f"i loop: {i_loop:.4f}")
+    F_B = abs(i_loop * B * L * cos(theta))
+    
+    domega_dt = (r*F_B) / I
     omega += domega_dt * dt
     theta += omega * dt
     
     
-    
-    i_loop = i_wire - V_back / R
-    F_B = i_loop * B * L
-    
     update_arrows(path, lf, rf)
     
 #    print(f"Omega*dt: {omega*dt}")
-#    print(f"RPM: {r*omega}")
+    print(f"omega: {omega:.4f}")
     RPM_graph.select()
-    gd.plot(t, r*omega)
+    gd1.plot(t, omega * 60 / (2*pi))
+    
+    flux_graph.select()
+    gd2.plot(t, B*A*cos(theta))
 
 
     arrow(axis = rotation_axis)
@@ -128,5 +138,8 @@ while t < 10:
     
     update_arrows(path, lf, rf)
     
-    commutators.rotate(axis=rotation_axis, angle = omega* dt)
+    commutators.rotate(axis=rotation_axis, angle = omega*dt)
+    
+    L *= 100
+    r *= 100
     
